24af5a3f0393351394feb3cec4894dd1af3a64d6
natashaval <natasha.valentina1998@gmail.com>
2021-01-12 21:29:20 +0700

    change color

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Moodify
+
+## Features:
+1. Track daily mood
+2. Set short-term goals and long-term goals
+3. User settings (?)
+4. Daily quotes
--- app/src/main/java/com/natashaval/moodify/MainActivity.kt
+++ app/src/main/java/com/natashaval/moodify/MainActivity.kt
@@ -7,20 +7,30 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.natashaval.moodify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    val navView: BottomNavigationView = findViewById(R.id.nav_view)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    val navView: BottomNavigationView = binding.navView
 
     val navController = findNavController(R.id.nav_host_fragment)
     // Passing each menu ID as a set of Ids because each
     // menu should be considered as top level destinations.
     val appBarConfiguration = AppBarConfiguration(
-        setOf(R.id.navigation_home, R.id.navigation_profile, R.id.navigation_notifications))
+        setOf(R.id.navigation_home, R.id.navigation_profile))
     setupActionBarWithNavController(navController, appBarConfiguration)
     navView.setupWithNavController(navController)
+
+    with(binding) {
+      fab.setOnClickListener {
+
+      }
+    }
   }
 }
